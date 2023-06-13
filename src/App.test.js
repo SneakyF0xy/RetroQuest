1f1d5a55468019065ed6dbeffa3363f8d7ce8067
SneakyF0xy <67873449+SneakyF0xy@users.noreply.github.com>
2023-06-13 18:33:56 +0300

    Remove trash

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import RetroQuest from './app/RetroQuest';
-
-test('renders learn react link', () => {
-  render(<RetroQuest />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
